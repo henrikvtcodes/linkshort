25c4443c951a17824e1c6f1266ee9d5404e2edda
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-09-08 22:21:47 -0400

    v1

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-n
+import { Router } from 'itty-router';
+import { customAlphabet } from 'nanoid';
+
+const router = Router();
+const nanoid = customAlphabet(
+  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+  6,
+);
+
+router.get('/:slug', async request => {
+  let link = await linkshort.get(request.params.slug);
+
+  if (link) {
+    return new Response(null, {
+      headers: { Location: link },
+      status: 301,
+    });
+  } else {
+    return new Response('Key not found', {
+      status: 404,
+    });
+  }
+});
+
+router.get('/', async request => {
+    return new Response(null, {
+      status: 200,
+    });
+});
+
+
+addEventListener('fetch', event => {
+  event.respondWith(router.handle(event.request))
+})
